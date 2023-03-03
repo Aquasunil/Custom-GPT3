6229cfd1776b38eadd1969fb0f589f00061c6778
Aquasunil <aquasunil429@gmail.com>
2023-03-04 00:56:05 +0500

    change in server.js again

--- server/server.js
+++ server/server.js
@@ -43,4 +43,4 @@ app.post('/', async(req,res)=>{
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000,()=>{console.log('running on port 5000')})
+app.listen(PORT,()=>{console.log('running on port 5000')})
